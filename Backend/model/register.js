aadad4371f102db8a4e34537807c9975c8460260
Viswanath <153046602+viswa1213@users.noreply.github.com>
2025-08-09 08:40:15 +0530

    Delete Backend directory

--- Backend/model/register.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const userschema = new mongoose.Schema({
-    UserId : {type : Number  ,unique : true},
-    Email : {type : String ,required : true, unique : true },
-    Password : {type :  String,required : true}
-});
-module.exports  = mongoose.model('users',userschema);
